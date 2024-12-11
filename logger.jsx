fbec527ca26d99edac8e550a57ab298490c51e7c
Hema Devika Putta <hemadevika210@gmail.com>
2024-12-11 18:03:45 +0100

    Logging Changes

--- logger.jsx
+++ logger.jsx
@@ -1,38 +1,23 @@
 // logger.js
-
 const logger = {
-
- log: (message) => {
-
- if (process.env.NODE_ENV !== "production") {
-
- console.log(`[LOG] ${new Date().toISOString()}: ${message}`);
-
- }
-
- },
-
- error: (message) => {
-
- if (process.env.NODE_ENV !== "production") {
-
- console.error(`[ERROR] ${new Date().toISOString()}: ${message}`);
-
- }
-
- },
-
- warn: (message) => {
-
- if (process.env.NODE_ENV !== "production") {
-
- console.warn(`[WARN] ${new Date().toISOString()}: ${message}`);
-
- }
-
- },
-
-};
-
-export default logger;
-
+    log: (message) => {
+      if (process.env.NODE_ENV !== "production") {
+        console.log(`[LOG] ${new Date().toISOString()}: ${message}`);
+      }
+    },
+  
+    error: (message) => {
+      if (process.env.NODE_ENV !== "production") {
+        console.error(`[ERROR] ${new Date().toISOString()}: ${message}`);
+      }
+    },
+  
+    warn: (message) => {
+      if (process.env.NODE_ENV !== "production") {
+        console.warn(`[WARN] ${new Date().toISOString()}: ${message}`);
+      }
+    },
+  };
+  
+  export default logger;
+  
